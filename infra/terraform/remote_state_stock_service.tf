abaec83721d8c5048c61bac87df97f00708b2d50
ThiagoFrederico <thiagofrederico3@gmail.com>
2026-02-21 21:18:42 -0300

    change env

--- infra/terraform/remote_state_stock_service.tf
+++ infra/terraform/remote_state_stock_service.tf
@@ -3,8 +3,7 @@ data "terraform_remote_state" "stock_service" {
 
   config = {
     bucket = "tf-state-challenge-bucket"
-    key = "v4/service-stock/dev/terraform.tfstate"
-    #key    = local.stock_service_state_path
+    key    = local.stock_service_state_path
     region = "us-east-2"
   }
 }
